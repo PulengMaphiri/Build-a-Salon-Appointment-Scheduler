cc0a55200357b5327bc2b87e36012473a02073e9
Puleng Maphiri <maphiripn@gmail.com>
2024-08-31 21:14:12 +0000

    second commit

--- salon.sh
+++ salon.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
+
 PSQL="psql --username=freecodecamp --dbname=salon --tuples-only -c"
 echo -e "\n~~~~~ MY SALON ~~~~~\n"
 
@@ -27,29 +28,30 @@ MAIN_MENU(){
   DISPLAY_SERVICES
   read SERVICE_ID_SELECTED
 
-  # check if SERVICE_REQUESTED is a number
+  # check if service requested is a number
    if [[ ! $SERVICE_ID_SELECTED =~ ^[0-9]+$ ]]
     then  
     MAIN_MENU "I could not find that service. What would you like today?"
     else
 
-  # get SERVICE_NAME if one exists
+  # get service name if one exists
   SERVICE_NAME=$($PSQL"SELECT name FROM services WHERE service_id=$SERVICE_ID_SELECTED")
-  # check is a SERVICE_NAME exisits
+  # check if service name exisits
   if [[ -z $SERVICE_NAME ]]
   then
-    # If not then return to the main menu
+    # If it doesn't then return to the main menu
     MAIN_MENU "I could not find that service. What would you like today?"
   fi
 
   echo -e "\nWhat's your phone number?"
   read CUSTOMER_PHONE
-
+  # Get customer id
   CUSTOMER_ID=$($PSQL"SELECT customer_id FROM customers WHERE phone='$CUSTOMER_PHONE'")
   if [[ -z $CUSTOMER_ID ]]
   then
     echo "I don't have a record for that phone number, what's your name?"
     read CUSTOMER_NAME
+    # add customer's name and phone
     INSERT_CUSTOMER_INFO=$($PSQL "INSERT INTO customers(name, phone) VALUES('$CUSTOMER_NAME', '$CUSTOMER_PHONE')")
     CUSTOMER_ID=$($PSQL"SELECT customer_id FROM customers WHERE name='$CUSTOMER_NAME'")
   else
@@ -58,7 +60,7 @@ MAIN_MENU(){
   
   echo -e "\nWhat time would you like your$SERVICE_NAME,$CUSTOMER_NAME?"
   read SERVICE_TIME
-  
+  # Insert appointment
   INSERT_APPOINTMENT=$($PSQL "INSERT INTO appointments(customer_id, service_id, time) VALUES($CUSTOMER_ID, $SERVICE_ID_SELECTED, '$SERVICE_TIME')")
 
  echo -e "\nI have put you down for a$SERVICE_NAME at $SERVICE_TIME, $CUSTOMER_NAME."
